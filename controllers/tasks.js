479fae645c3a96006a8d16f77353d08cc2726338
Stephen Muñez <sjtmunez@gmail.com>
2024-07-23 14:45:13 +0800

    setup: db create task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,12 @@
+const Task = require('../models/Task');
+
 const getAllTasks = (req, res) => {
   res.send('Get all tasks');
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.json({ task });
 };
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
