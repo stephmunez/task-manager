95059c4626f617908aa658409d9d6e58c91cb6d9
Stephen Muñez <sjtmunez@gmail.com>
2024-07-31 15:32:48 +0800

    refactor: controllers

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.use(express.json());
 app.use('/api/v1/tasks', tasks);
 app.use(notFound);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ message: err.message });
+});
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
