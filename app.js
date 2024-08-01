9790c6ea15657ca6114a9c848e8654067f92b00e
Stephen Muñez <sjtmunez@gmail.com>
2024-08-01 14:59:33 +0800

    setup: port variable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 const notFound = require('./middleware/not-found');
